ac66247b0cfae2ea54e859f4b981cdaea7405fb3
koder31py <bogdan.mamcev2012@gmail.com>
2025-11-23 15:14:10 +0600

    ls2

--- movies/urls.py
+++ movies/urls.py
@@ -4,8 +4,3 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
 ]
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('movies.urls')),
-]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def index(request):
-    return render(request, 'movies/index.html')
+    movies = [
+        {
+            'title': 'THe Bad Guys',
+            'description': 'Мультфильм «Плохие парни» рассказывает о банде антропоморфных животных-преступников, которые ради спасения от тюрьмы решают притвориться хорошими, но в итоге обнаруживают, что им нравится быть добрыми, и предотвращают злодейский план',
+            'poster': 'the.jpg',
+            'trailer': 'https://youtu.be/d8XnK14-x_A?si=ubiwVMYp3Z1JRgl1',
+        },
+        {
+            'title': 'МегаМозг',
+            'description': 'В мультфильме «Мегамозг» суперзлодей побеждает супергероя, но, потеряв смысл жизни, создает нового противника, который сам становится угрозой для города, вынуждая Мегамозга стать настоящим героем.',
+            'poster': 'mozg.jpg',
+            'trailer': 'https://youtu.be/AFwrmkAHEk4?si=uVdTgMNnkqcX8Hyh',
+        },
+        {
+            'title': 'Гарри Потер и философский камень',
+            'description': 'Гарри Поттер узнает, что он волшебник, и в школе Хогвартс предотвращает похищение философского камня злодеем.',
+            'poster': 'mrpoter.jpg',
+            'trailer': 'https://youtu.be/AFwrmkAHEk4?si=uVdTgMNnkqcX8Hyh',
+        }
+    ]
+    return render(request, 'movies/index.html', {'movies': movies})
